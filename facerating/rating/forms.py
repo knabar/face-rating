47c8265d6f60e94c6bd72e902a7ee7f5319e3a52
knabar <knabar@1056e5d0-9bb3-11dd-8a8f-13d9fc7cf837>
2009-03-25 20:53:13 +0000

    Updated rating forms

--- facerating/rating/forms.py
+++ facerating/rating/forms.py
@@ -1,3 +1,4 @@
+from django.forms.formsets import formset_factory, BaseFormSet
 from django import forms
 from django.shortcuts import render_to_response
 from django.contrib.formtools.wizard import FormWizard
@@ -52,6 +53,15 @@ class FaceRatingWizard(FormWizard):
         raise Exception()
 
 
+class IterableBaseFormSet(BaseFormSet):
+    def __iter__(self):
+        for form in self.forms:
+            for f in form:
+                yield f
+        for f in self.management_form:
+            yield f
+
+
 class Part1Wizard(FaceRatingWizard):
 
     class Part1RatingForm(forms.Form):
@@ -60,32 +70,38 @@ class Part1Wizard(FaceRatingWizard):
 #        p2 = ratingField('Asian prototypicality')
 #        p3 = ratingField('American prototypicality')
         i = forms.CharField(widget=FaceImageWidget())
-    
+        
     def __init__(self, count):
-        forms = [Part1Wizard.Part1RatingForm] * min(count, len(self._get_filelist()))
+        
+        Part1RatingFormSet = formset_factory(Part1Wizard.Part1RatingForm, extra=0, formset=IterableBaseFormSet)
+        
+        forms = [Part1RatingFormSet] * int(round(min(count, len(self._get_filelist())) / 10.0 + 0.49999))
+#        forms = [Part1Wizard.Part1RatingForm] * min(count, len(self._get_filelist()))
+        self.image_count = count
         super(Part1Wizard, self).__init__(forms)
             
     def _get_images(self, request):
         images = request.session.get('part1images', None)
         if not images:
             images = self._get_filelist()
-            images = map(lambda i: {'i': 'faces/' + i}, random.sample(images, min(len(images), len(self.form_list))))
+            images = map(lambda i: {'i': 'faces/' + i}, random.sample(images, min(len(images), self.image_count)))
             request.session['part1images'] = images
         return images
             
     def get_form(self, step, data=None):
-        return self.form_list[step](data, prefix=self.prefix_for_step(step),
-                                    initial=self._get_images(sys._getframe(1).f_locals.get('request'))[step])
+        initial = self._get_images(sys._getframe(1).f_locals.get('request'))[step*10:step*10+10]
+        return self.form_list[step](data, prefix=self.prefix_for_step(step), initial=initial)
 
     def done(self, request, form_list):
         submission = Submission.objects.get(id=request.session['submission'])
-        for form in form_list:
-            RatingResponse.objects.create(submission=submission,
-                    prototypicality_asam=form.cleaned_data['p1'],
-#                    attractiveness=form.cleaned_data['a'],
-#                    prototypicality_as=form.cleaned_data['p2'],
-#                    prototypicality_am=form.cleaned_data['p3'],
-                    face=os.path.splitext(os.path.basename(form.cleaned_data['i']))[0])
+        for formset in form_list:
+            for form in formset.forms:
+                RatingResponse.objects.create(submission=submission,
+                        prototypicality_asam=form.cleaned_data['p1'],
+    #                    attractiveness=form.cleaned_data['a'],
+    #                    prototypicality_as=form.cleaned_data['p2'],
+    #                    prototypicality_am=form.cleaned_data['p3'],
+                        face=os.path.splitext(os.path.basename(form.cleaned_data['i']))[0])
         submission.progress = 1
         submission.save() 
         return HttpResponseRedirect(reverse("part2intro"))
@@ -104,9 +120,17 @@ class Part2Wizard(FaceRatingWizard):
         i1 = forms.CharField(widget=FaceImageWidget())
         i2 = forms.CharField(widget=FaceImageWidget())
 
+
     def __init__(self, count):
+
         files = self._get_filelist()
-        forms = [Part2Wizard.Part2RatingForm] * min(count, len(files) * (len(files) - 1))
+
+        Part2RatingFormSet = formset_factory(Part2Wizard.Part2RatingForm, extra=0, formset=IterableBaseFormSet)
+        forms = [Part2RatingFormSet] * int(round(min(count, len(files) * (len(files) - 1)) / 10.0 + 0.49999))
+
+        self.image_count = count
+
+#        forms = [Part2Wizard.Part2RatingForm] * min(count, len(files) * (len(files) - 1))
         super(Part2Wizard, self).__init__(forms)
             
     def _get_pairs(self, request):
@@ -118,7 +142,7 @@ class Part2Wizard(FaceRatingWizard):
                 return {'i1': 'faces/' + images[x],
                         'i2': 'faces/' + images[(x + random.randint(1, len(images) - 2)) % len(images)]}
             pairs = []
-            while len(pairs) < len(self.form_list):
+            while len(pairs) < self.image_count:
                 p = gen_pair()
                 if not p in pairs:
                     pairs.append(p)
@@ -127,15 +151,16 @@ class Part2Wizard(FaceRatingWizard):
             
     def get_form(self, step, data=None):
         return self.form_list[step](data, prefix=self.prefix_for_step(step),
-                                    initial=self._get_pairs(sys._getframe(1).f_locals.get('request'))[step])
+                                    initial=self._get_pairs(sys._getframe(1).f_locals.get('request'))[step*10:step*10+10])
 
     def done(self, request, form_list):
         submission = Submission.objects.get(id=request.session['submission'])
-        for form in form_list:
-            SimilarityResponse.objects.create(submission=submission,
-                similarity=form.cleaned_data['s'],
-                face1=os.path.splitext(os.path.basename(form.cleaned_data['i1']))[0],
-                face2=os.path.splitext(os.path.basename(form.cleaned_data['i2']))[0])
+        for formset in form_list:
+            for form in formset.forms:
+                SimilarityResponse.objects.create(submission=submission,
+                    similarity=form.cleaned_data['s'],
+                    face1=os.path.splitext(os.path.basename(form.cleaned_data['i1']))[0],
+                    face2=os.path.splitext(os.path.basename(form.cleaned_data['i2']))[0])
         
         submission.progress = 2
         submission.save()                
